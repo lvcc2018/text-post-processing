43fd1ac09b8671a14129a5979a95c3d9f1c52e9f
Claire Lv <thulvcc2017@gmail.com>
2022-09-08 10:23:46 +0800

    Update preprocess the lot

--- DataProcessor/DataProcessor.py
+++ DataProcessor/DataProcessor.py
@@ -8,6 +8,7 @@
 '''
 import random
 import numpy as np
+import json
 
 
 class DataProcessor(object):
@@ -19,49 +20,59 @@ class DataProcessor(object):
     def __init__(self):
         pass
 
+    def _read_jsonl(self, file_path):
+        return [json.loads(line.strip) for line in open(file_path, 'r').readlines()]
+    
+    def _write_jsonl(self, obj, file_path):
+        f = open(file_path, 'w', encoding='utf-8')
+        for line in obj:
+            f.write(json.dumps(line, ensure_ascii=False) + '\n')
+
+    def preprocess_data(self, data_path='../data/LOT', save_path='../data'):
+        """Preprocess the LOT dataset.
+        
+        Args:
+            data_path: path to the LOT dataset.
+        """
+        data = []
+        splits = ['train', 'valid', 'test']
+        for split in splits:
+            for line in self._read_jsonl(data_path + f'/outgen/{split}.jsonl'):
+                data.append(line['story'])
+            for line in self._read_jsonl(data_path + f'/clozet/{split}.jsonl'):
+                pl = {i:line[f'plot{str(i)}'] for i in [0,1]}
+                data.append(line['story'].replace('<mask>', pl[line['label']]))
+            for line in self._read_jsonl(data_path + f'/plotcom/{split}.jsonl'):
+                data.append(line['story'].replace('<mask>', line['plot']))
+            for line in self._read_jsonl(data_path + f'/senpos/{split}.jsonl'):
+                st = line['story'].split('<mask>')
+                st = st[:line['label']+1] + line['sentence'] + st[line['label']+1:]
+        data = list(set(data))
+        self._write_jsonl(data, save_path + '/data.jsonl')          
+        
+        
+
     def random_span_mask(self, tokens: list, max_span_num: int, mask_ratio: float = 0.2):
         """Span masks randomly for the origin text.
 
         Args:
-            text: list of the tokens to be mask. 
-            max_span_num: maximum of the span number.
-            mask_ratio: ratio of the text to be masked.
+            
         Returns:
-            span_idx_pair: list of the start idx and end idxs.
+            
         Raises:
         """
-        text_length = len(tokens)
-        mask_length = round(text_length * mask_ratio)
-        span_number = random.randint(1, max(1, max_span_num))
-
-        single_span_length = round(mask_length / span_number)
-        span_idx_pair = []
-
-        for i in range(span_number):
-            random_span_length = min(max(1, np.random.normal(
-                single_span_length, single_span_length * 0.1, 1)[0]), round(text_length/span_number))
-
-            def get_start_idx():
-                res = []
-                for j in range(len(tokens)-random_span_length):
-                    flag = True
-                    for k in span_idx_pair:
-                        if j in range(k[0], k[1]):
-                            flag = False
-                            break
-                    if flag:
-                        res.append(j)
-                return res
-            start_idx = random.choice(get_start_idx())
-            span_idx_pair.append([start_idx, start_idx + random_span_length])
-        return span_idx_pair
+        pass
 
     def process_text(self, text):
         """Formulate the text into training instance of CPM-3
-        
+
         Args:
         Returns:
         Raises:
         """
-        
-        
+        pass
+
+
+if __name__ == '__main__':
+    dp = DataProcessor()
+    dp.preprocess_data()
