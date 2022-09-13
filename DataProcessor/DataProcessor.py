c87ab8ba413ec7c7451d30ce79c689e52fffbc51
thulvcc <thulvcc2017@gmail.com>
2022-09-13 16:58:01 +0800

    Update functions

--- DataProcessor/DataProcessor.py
+++ DataProcessor/DataProcessor.py
@@ -65,7 +65,7 @@ class DataProcessor(object):
     def random_span_mask(self, text):
         """Span masks randomly for the origin text.
         """
-        span_len = random.randint(1, len(text)-1)
+        span_len = random.randint(1, round(len(text)*0.3))
         start_idx = random.randint(1, len(text)-span_len)
         source = [text[:start_idx], text[start_idx+span_len:]]
         target = text[start_idx:start_idx+span_len]
@@ -86,8 +86,8 @@ class DataProcessor(object):
             pseudo_data.append(self.process_text([d[:20],''], d[20:], 'lm'))
         self._write_jsonl(pseudo_data, save_path)
     
-    def generate_pl_pseudp_data(self, data_path='../data/data.txt', save_path='../data/pl_pseudo_data.txt'):
-        """Generate the pl pseudo data for training the CPM-3 model.
+    def generate_pc_pseudp_data(self, data_path='/home/lvcc/CPM-3/data/data.txt', save_path='../CPM-3/data/pc_pseudo_data.txt'):
+        """Generate the pc pseudo data for training the CPM-3 model.
         """
         data = self._read_txt(data_path)
         pseudo_data = []
@@ -96,6 +96,17 @@ class DataProcessor(object):
             pseudo_data.append(self.process_text(source, target, 'lm'))
         self._write_jsonl(pseudo_data, save_path)
     
+    def generate_prefix_pc_pseudo_data(self, source_data_path, data_path, save_path):
+        source_data = self._read_jsonl(source_data_path)
+        data = self._read_txt(data_path)
+        pseudo_data = []
+        assert(len(source_data) == len(data))
+        for i in range(len(data)):
+            source, target = self.random_span_mask(data[i])
+            pseudo_data.append(self.process_text([source_data[i]['source'][0]+source[0], source[1]], target, 'lm'))
+        self._write_jsonl(pseudo_data, save_path)
+
+
     def split_dataset(self, data_path):
         data = self._read_jsonl(data_path)
         random.shuffle(data)
@@ -106,12 +117,38 @@ class DataProcessor(object):
         self._write_jsonl(val_data, data_path.replace('.txt', '_val.txt'))
         self._write_jsonl(test_data, data_path.replace('.txt', '_test.txt'))
 
+    def generate_seq_label_pseudo_data(self, source_data_path, output_data_path, save_path):
+        """利用CPM-3的填空能力生成结果，然后结合原始数据，得到infix伪造的数据
+        """
+        
+        
+        source = [json.loads(i) for i in open(source_data_path, 'r').readlines()]
+        output = [i.strip() for i in open(output_data_path).readlines()]
+        prefix = [s['source'][0] for s in source]
+        postfix = [s['source'][1] for s in source]
+        infix = [output[i][:(len(output[i])-len(postfix[i]))] for i in range(len(output))]
+        data = [{'prefix':prefix[i], 'postfix':postfix[i], 'infix':infix[i], 'truth':source[i]['target']} for i in range(len(prefix))]
+        self._write_jsonl(data, save_path)
+    
+    def get_max_ppl_seq(self, nums, min_len = 3):
+        if nums < min_len:
+            return [-1, -1]
+        max_average = 0
+        for start_idx in range(len(nums)-min_len):
+            for end_idx in range(i+min_len, len(nums)):
+                average_num = sum(nums[start_idx:end_idx])
+                if average_num > max_average:
+                    max_average = average_num
+                    res = [start_idx, end_idx]
+        return max_average, res
+        
 
 
 if __name__ == '__main__':
     dp = DataProcessor()
-    # dp.preprocess_data()
-    # dp.generate_lm_pseudo_data()
-    # dp.generate_pl_pseudp_data()
-    dp.split_dataset('../data/lm_pseudo_data.txt')
-    dp.split_dataset('../data/pl_pseudo_data.txt')
+    for s in ['train','val','test']:
+        dp.generate_seq_label_pseudo_data(
+            f'/home/lvcc/CPM-3/data/pc_data/pc_pseudo_data_0.3_{s}.txt',
+            f'/home/lvcc/CPM-3/data/span_predict_train_data/{s}.txt',
+            f'/home/lvcc/CPM-3/data/span_predict_train_data/{s}_pseudo.txt'
+        )
